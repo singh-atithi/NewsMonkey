e3ad3f962bd6c002aa9b1217e8bc27d213c44d3e
singh-atithi <singhatithi02@gmail.com>
2024-08-07 22:31:38 +0530

    App.js format and add element

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import './App.css';
+import "./App.css";
 
-import React, { Component } from 'react'
-import Navbar from './Components/Navbar';
-import News from './Components/News';
+import React, { Component } from "react";
+import Navbar from "./Components/Navbar";
+import News from "./Components/News";
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <Navbar/>
-        <News/>
+        <Navbar />
+        <News />
       </div>
-    )
+    );
   }
 }
