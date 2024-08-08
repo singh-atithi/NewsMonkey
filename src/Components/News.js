d6ca6e3a3dbbc2c868603aad30872882d63569da
singh-atithi <singhatithi02@gmail.com>
2024-08-08 21:18:28 +0530

    Made pg-1 sourse

--- src/Components/News.js
+++ src/Components/News.js
@@ -2,76 +2,49 @@ import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 
 export class News extends Component {
-  articles = [
-    {
-      source: {
-        id: "espn-cric-info",
-        name: "ESPN Cric Info",
-      },
-      author: null,
-      title:
-        "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-      description:
-        "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-      url: "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-      urlToImage:
-        "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-      publishedAt: "2020-04-27T11:41:47Z",
-      content:
-        "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]",
-    },
-    {
-      source: {
-        id: "espn-cric-info",
-        name: "ESPN Cric Info",
-      },
-      author: null,
-      title:
-        "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-      description:
-        "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-      url: "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-      urlToImage:
-        "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-      publishedAt: "2020-03-30T15:26:05Z",
-      content:
-        "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]",
-    },
-  ];
+  articles = [];
+
   constructor() {
     super();
     this.state = {
-      articles: this.articles,
+      articles:[
+       ],
       loading: false,
     };
   }
+
+  async componentDidMount() {
+    let url = "https://feeds.bbci.co.uk/news/technology/rss.xml";
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData)
+    this.setState({ articles: parsedData.data });
+//     BASE_URL = ""
+// top_headlines_api = "<BASE_URL>/"
+// everything_api = 
+  }
+
   render() {
     return (
       <div className="container my-3">
         <h2 className="display-4 mb-3">NewsMonkey - Top Headlines</h2>
-
         <div className="row">
-          <div className="col-md-4">
-            <NewsItem
-              title="title"
-              description="description"
-              imageUrl="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg"
-            />
-          </div>
-          <div className="col-md-4">
-            <NewsItem
-              title="title"
-              description="description"
-              imageUrl="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg"
-            />
-          </div>
-          <div className="col-md-4">
-            <NewsItem
-              title="title"
-              description="description"
-              imageUrl="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg"
-            />
-          </div>
+          {this.state.articles.map((element) => {
+            // Only render NewsItem if title is not null, not empty, and not "Removed"
+            if (element.title && element.title !== "[Removed]") {
+              return (
+                <div className="col-md-4" key={element.url}>
+                  <NewsItem
+                    title={element.title ? element.title.slice(0, 40) : ""}
+                    description={element.description ? element.description.slice(0, 60) : ""}
+                    imageUrl={element.urlToImage?element.urlToImage:"https://uploads.metroimg.com/wp-content/uploads/2024/08/06182234/GettyImages-632549212.jpg"}
+                    newsUrl={element.url}
+                  />
+                </div>
+              );
+            }
+            return null;
+          })}
         </div>
       </div>
     );
